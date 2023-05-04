f884860160ecb84179534f53c440d5e1460f02ad
serkazjesest <serkazjesest@gmail.com>
2023-05-04 13:32:06 +0300

    init task_02 commit

--- internal/customer.go
+++ internal/customer.go
@@ -1,12 +1,26 @@
 package internal
 
+import "errors"
+
+const DEFAULT_DISCOUNT = 500
+
 type Customer struct {
-	Name         string
-	Age          int
-	Balance      int
-	Debt         int
-	Discount     bool
-	CalcDiscount func() (int, error)
+	Name     string
+	Age      int
+	Balance  int
+	Debt     int
+	discount bool
+}
+
+func (cust *Customer) CalcDiscount() (int, error) {
+	if !cust.discount {
+		return 0, errors.New("discount not available")
+	}
+	result := DEFAULT_DISCOUNT - cust.Debt
+	if result < 0 {
+		return 0, nil
+	}
+	return result, nil
 }
 
 func NewCustomer(name string, age int, balance int, debt int, discount bool) *Customer {
@@ -15,6 +29,6 @@ func NewCustomer(name string, age int, balance int, debt int, discount bool) *Cu
 		Age:      age,
 		Balance:  balance,
 		Debt:     debt,
-		Discount: discount,
+		discount: discount,
 	}
 }
